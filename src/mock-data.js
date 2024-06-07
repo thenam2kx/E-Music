e7604470fc4e16969bf2f3b95df440cea2b40f71
Thenam2kx <thenam2kx@gmail.com>
2024-06-08 04:39:49 +0700

    upload audio and mock-data

---
+++ src/mock-data.js
@@ -0,0 +1,322 @@
+export const Musics =[
+  {
+    'id': 1,
+    'title': 'nho-gia-dinh-remix',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'nho-gia-dinh-remix.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'nho-gia-dinh',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'nho-gia-dinh.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'phia-sau-to-dola',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'phia-sau-to-dola.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'com-doan-vien',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'com-doan-vien.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'con-trai-cung',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'con-trai-cung.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'khoc-mot-minh',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'khoc-mot-minh.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'hoa-no-ben-duong',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'hoa-no-ben-duong.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'sau-loi-tu-khuoc',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'sau-loi-tu-khuoc.m4a',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'mua-thang-sau',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'mua-thang-sau.m4a',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'lung-lo',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'lung-lo.m4a',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'loi-tam-biet-chua-noi',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'loi-tam-biet-chua-noi.m4a',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'lan-cuoi',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'lan-cuoi.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'hoa-no-khong-mau',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'hoa-no-khong-mau.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'ai-roi-cung-se-khac',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'ai-roi-cung-se-khac.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'co-don-tren-sofa',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'co-don-tren-sofa.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'co-mot-nguoi-van-doi',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'co-mot-nguoi-van-doi.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'cu-voi-vang',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'cu-voi-vang.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'cuoc-vui-co-don',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'cuoc-vui-co-don.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'dem-trang-tinh-yeu',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'dem-trang-tinh-yeu.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'hom-nay-em-cuoi-roi',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'hom-nay-em-cuoi-roi.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'moi-tinh-chieu-mua-bay',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'moi-tinh-chieu-mua-bay.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'mong-manh-tinh-ve',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'mong-manh-tinh-ve.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'mua-thang-sau-remix',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'mua-thang-sau-remix.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'nguoi-yeu-cu',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'nguoi-yeu-cu.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'song-xa-anh-chang-de-dang',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'song-xa-anh-chang-de-dang.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'tat-ca-se-thay-anh',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'tat-ca-se-thay-anh.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'thanh-pho-buon',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'thanh-pho-buon.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'thu-cuoi',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'thu-cuoi.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'tim-lai-bau-troi',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'tim-lai-bau-troi.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'trang-giay-trang',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'trang-giay-trang.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'tung-yeu',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'tung-yeu.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  },
+  {
+    'id': 1,
+    'title': 'ngay-mai-nguoi-ta-lay-chong',
+    'artist': 'artist artist',
+    'album': '',
+    'thumbnil_url': 'https://picsum.photos/100/100',
+    'url': 'ngay-mai-nguoi-ta-lay-chong.mp3',
+    'genre': 'Pop',
+    'duration': ''
+  }
+]
